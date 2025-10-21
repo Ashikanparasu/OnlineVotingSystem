2fb5b32b0d2944b9b515e633486af141e60ecd3a
Ashikanparasu <ashikanparasu@gmail.com>
2025-10-21 14:40:42 +0530

    Syntax

---
+++ frontend/src/home/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './Nav.jsx';
+import Main from './Main.jsx';
+import Footer from './Footer.jsx';
+
+function Homepage(){
+    return(
+        <>
+        <Nav />
+        <Main />
+        <Footer />
+
+        </>
+    );
+}
+
+export default Homepage;
