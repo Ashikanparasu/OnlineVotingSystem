6daeba1fc0705ccbb19dc81e850f66a414da6ee8
Ashikanparasu <ashikanparasu@gmail.com>
2025-10-21 16:06:34 +0530

    votemate logo

--- frontend/src/home/Homepage.jsx
+++ frontend/src/home/Homepage.jsx
@@ -5,12 +5,17 @@ import Footer from './Footer.jsx';
 
 function Homepage(){
     return(
-        <>
-        <Nav />
-        <Main />
-        <Footer />
-
-        </>
+        <div style={{ position: 'relative', minHeight: '100vh' }}>
+            <div 
+                className="background-container" 
+                style={{ backgroundImage: 'url(/Background.jpg)' }}
+            />
+            <div style={{ position: 'relative', zIndex: 1 }}>
+                <Nav />
+                <Main />
+                <Footer />
+            </div>
+        </div>
     );
 }
 
--- frontend/src/home/Nav.jsx
+++ frontend/src/home/Nav.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Nav(){
-    return (<h1> Nav </h1>);
+    return (
+        <>
+        <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src="../public/vote.png" width="30" height="24" className="d-inline-block align-text-top" />
+        V O T M A T E
+    </a>
+  </div>
+</nav>
+        </>
+    );
 }
 
 export default Nav;
